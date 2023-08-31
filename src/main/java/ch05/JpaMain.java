7f1102ec9eace65cc84fd43fc8cf224ad9cbc3f7
JiYoon0712 <tkstpqpfldk9@naver.com>
2023-08-31 22:26:04 +0900

    jpa

---
+++ src/main/java/ch05/JpaMain.java
@@ -0,0 +1,38 @@
+package ch05;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member6 member = new Member6();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        // 실제 애플리케이션이 끝나면 EntityManagerFactory를 닫아줘야 함.
+        emf.close();
+    }
+}
